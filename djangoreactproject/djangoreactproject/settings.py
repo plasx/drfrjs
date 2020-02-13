0fd0510cc2a33c99e806353dcabd32b0e95d979d
Daniel Rivera <daniel.d.rivera@us.pwc.com>
2020-02-13 17:15:47 -0500

    added cors middleware and localhost:3000 whitelist

--- djangoreactproject/djangoreactproject/settings.py
+++ djangoreactproject/djangoreactproject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -47,7 +49,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
 ]
+
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ORIGIN_WHITELIST = (
+       'localhost:3000',
+)
 
 ROOT_URLCONF = 'djangoreactproject.urls'
 
