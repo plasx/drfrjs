1dad63cf3251d63a85f3019a9a35b0b60c591944
Daniel Rivera <daniel.d.rivera@us.pwc.com>
2020-02-14 12:25:36 -0500

    installed react router dom and setup baselayout

--- djangoreactproject/frontend/src/App.js
+++ djangoreactproject/frontend/src/App.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
+
+import  CustomersList from './CustomersList'
+import  CustomerCreateUpdate  from './CustomerCreateUpdate'
 import './App.css';
+
+const BaseLayout = () => (
+  <div className="container-fluid">
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="#">Django React Demo</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div className="navbar-nav">
+      <a className="nav-item nav-link" href="/">CUSTOMERS</a>
+      <a className="nav-item nav-link" href="/customer">CREATE CUSTOMER</a>
+
+    </div>
+  </div>
+</nav>  
+
+    <div className="content">
+      <Route path="/" exact component={CustomersList} />
+      <Route path="/customer/:pk"  component={CustomerCreateUpdate} />
+      <Route path="/customer/" exact component={CustomerCreateUpdate} />
+
+    </div>
+
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <BaseLayout/>
+      </BrowserRouter>
     );
   }
 }
